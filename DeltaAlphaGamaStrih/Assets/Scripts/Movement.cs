f222ba7fd2711f266135a07942f63ed3a27d9522
tyqFaradey <125170982+tyqFaradey@users.noreply.github.com>
2023-02-13 14:58:15 +0700

    Movement added

---
+++ DeltaAlphaGamaStrih/Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int speed;
+
+    private float moveInputH;
+    private float moveInputV;
+
+    private KBGPController controls;
+
+    private void Awake()
+    {
+        controls = new KBGPController();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+
+    void Update()
+    {
+        Movement0();
+    }
+
+    private void Movement0()
+    {
+        moveInputH = controls.Main.MoveH.ReadValue<float>();
+        moveInputV = controls.Main.MoveV.ReadValue<float>();
+
+        if (moveInputV > 0) // W
+        {
+            rb.velocity = new Vector2(rb.velocity.x, moveInputV * speed / 10);
+        }
+
+        else if (moveInputV < 0) // S
+        {
+
+            rb.velocity = new Vector2(rb.velocity.x, moveInputV * speed / 10);
+        }
+
+        else if (moveInputH > 0) // D
+        {
+            rb.velocity = new Vector2(moveInputH * speed / 10, rb.velocity.y);
+        }
+
+        else if (moveInputH < 0) // A
+        {
+            rb.velocity = new Vector2(moveInputH * speed / 10, rb.velocity.y);
+        }
+
+    }
+}
